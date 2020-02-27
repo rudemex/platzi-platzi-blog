9b20a237b8a12232c6f0e07ee262c1ae2b6e021e
Maximiliano Delgado <mdelgado@baufest.com>
2020-02-27 16:21:01 -0300

    add redux in component son

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -7,7 +7,7 @@ export const traerTodos = () => async dispatch => {
   });
   try {
     const respuesta = await axios.get(
-      "https://jsonplaceholder.typicode.com/userss"
+      "https://jsonplaceholder.typicode.com/users"
     );
     dispatch({
       type: TRAER_TODOS,
--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -4,7 +4,8 @@ import { connect } from "react-redux";
 import * as usuariosActions from "../../actions/usuariosActions";
 
 import Spinner from "../General/Spinner";
-import Fatal from "../General/Fatal"
+import Fatal from "../General/Fatal";
+import Tabla from "./Tabla";
 
 class Usuarios extends Component {
   async componentDidMount() {
@@ -16,36 +17,21 @@ class Usuarios extends Component {
       return <Spinner />;
     }
 
-    if (this.props.error){
-        return <Fatal msg={this.props.error}/>;
+    if (this.props.error) {
+      return <Fatal msg={this.props.error} />;
     }
-    return (
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>{this.ponerFilas()}</tbody>
-      </table>
-    );
+    return <Tabla />;
   };
-
-  ponerFilas = () =>
-    this.props.usuarios.map(usuario => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ));
 
   render() {
     //console.log(this.props.cargando)
     //console.log(this.props.error)
-    return <div>{this.ponerContenido()}</div>;
+    return (
+      <div>
+        <h1>Usuarios</h1>
+        {this.ponerContenido()}
+      </div>
+    );
   }
 }
 
